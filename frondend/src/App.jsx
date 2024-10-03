24d563066f8b0efb2b5bb016d58006a5352f69c3
KavinElango0 <ekavin0072001@gmail.com>
2024-10-03 22:36:25 +0530

    Navbar&fuecommit

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Order from './pages/Order'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div></div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Collection' element={<Collection/>} />
+        <Route path='/About' element={<About/>} />
+        <Route path='/Contact' element={<Contact/>} />
+        <Route path='/Place-Order' element={<PlaceOrder/>} />
+        <Route path='/Product/:productId' element={<Product/>} />
+        <Route path='/Cart' element={<Cart/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/Order' element={<Order/>} />
+      </Routes>
+      
+
+    </div>
   )
 }
 
