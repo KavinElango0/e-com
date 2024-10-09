f8f2e84b2d0da4472d86556f661c9703fdfa898c
KavinElango0 <ekavin0072001@gmail.com>
2024-10-09 10:50:46 +0530

    BestSellerProduct Add

--- frondend/src/components/Productitem.jsx
+++ frondend/src/components/Productitem.jsx
@@ -5,14 +5,16 @@ import {Link} from 'react-router-dom'
 const Productitem = ({id,image,name,price}) => {
 
      const {currency} = useContext(ShopContext);
+     const productimage = image && image.length > 0 ? image[0] : '/placeholder-image.jpg'; 
 
   return (
-    <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+    <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`} >
         <div className='overflow-hidden'>
-            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
+            <img className='hover:scale-110 transition ease-in-out' src={productimage} alt="" />
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency}{price}</p>
+       
     </Link>
   )
 }
