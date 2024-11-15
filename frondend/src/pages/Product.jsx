2466cf0a5567642fcf936516ff1250c56c3eba7e
KavinElango0 <ekavin0072001@gmail.com>
2024-11-15 22:18:54 +0530

    searchbar Add

--- frondend/src/pages/Product.jsx
+++ frondend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react'
 import { useParams }  from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 
 const Product = () => {
@@ -10,6 +11,7 @@ const Product = () => {
     const { products,currency } = useContext(ShopContext);
     const [productData,setProductData] = useState(false)
     const [image,setImage] = useState('')
+    const [size,setSize] = useState('')
 
     const fetchProductData = async () => {
 
@@ -59,10 +61,41 @@ const Product = () => {
             <img src={assets.star_dull_icon} alt="" className="w-3 5" />
             <p className='pl-2'>(122)</p>
         </div>
-        <p>{currency}</p>
+        <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+        <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+        <div className='flex flex-col gap-4 my-8'>
+            <p>Select Size</p>
+            <div className='flex gap-2'>
+                {productData.sizes.map((item,index)=>(
+                  <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-800' : ''}`} key={index}>{item}</button>
+                ))}
+            </div>
+        </div>
+         <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+         <hr className='mt-8 sm:w-4/5'/>
+         <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+          <p>100% original product.</p>
+          <p>Cash on delivery is available on this product.</p>
+          <p>Easy return and exhange policy within 7 days.</p>
+         </div>
       </div>
      </div>
 
+     {/*----- Description & Review Section ----- */}
+     <div className='mt-20'>
+       <div className='flex'>
+        <b className='border px-5 py-3 text-sm'>Description</b>
+        <p className='border px-5 py-3 text-sm'>Reviews(122)</p>
+       </div>
+       <div className='flex flex-col gap-4 border px-6 tex-sm text-gray-500'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab maiores impedit quae reiciendis quod ut explicabo, maxime molestias quisquam, sit repellat laboriosam animi error, atque similique quaerat unde officiis alias!</p>
+        <p>Discover a world of premium products, unbeatable deals, and seamless shopping experiences. From top-notch gadgets to everyday essentials, we’ve got everything you need—all in one place.</p>
+       </div>
+     </div>
+
+     {/*----- display related products ------*/}
+
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : <div className=" opacity-0"></div>
 }
